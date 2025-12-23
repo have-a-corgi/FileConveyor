fa847ee0a495797f7aed0084b46badb76f52870f
dnikiforov1967 <dnikiforov@mail.ru>
2025-12-23 23:21:01 +0300

    +liquibase

---
+++ src/test/java/org/nda/osp/LiquibaseTest.java
@@ -0,0 +1,16 @@
+package org.nda.osp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("liqui")
+public class LiquibaseTest {
+
+    @Test
+    void testOfStart() {
+
+    }
+
+}
